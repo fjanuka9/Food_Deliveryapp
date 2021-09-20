b0a5f82ce8452d9fb92fc25587e5198293079c76
fjanuka9 <90580551+fjanuka9@users.noreply.github.com>
2021-09-20 22:03:13 +0530

    images

---
+++ lib/screens/splash_screen/splash_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/food.png',
+            ),
+            Text(
+              'No waiting for food',
+              style: GoogleFonts.poppins(
+                fontSize: 14,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
