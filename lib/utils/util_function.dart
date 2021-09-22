92920d70e853efbc3d89f897cafa4ed5507d9ba1
fjanuka9 <90580551+fjanuka9@users.noreply.github.com>
2021-09-22 11:04:59 +0530

    go_back

--- lib/utils/util_function.dart
+++ lib/utils/util_function.dart
@@ -1,7 +1,14 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class UtilFunction {
   static void navigateTo(BuildContext context, Widget widget) {
     Navigator.push(context, MaterialPageRoute(builder: (context) => widget));
+  }
+
+//go back
+  static void goBack(BuildContext context) {
+    Navigator.of(context).pop();
   }
 }
