22f1a9779a0309347e28c92b4265cc59b1ad98e1
fjanuka9 <90580551+fjanuka9@users.noreply.github.com>
2021-09-23 20:22:17 +0530

    ud

---
+++ lib/screens/login_screen/login_screen.dart
@@ -0,0 +1,233 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:foodapp/components/custom_button.dart';
+import 'package:foodapp/components/custom_header.dart';
+import 'package:foodapp/components/custom_text_field.dart';
+import 'package:foodapp/screens/login_screen/register_screen.dart';
+import 'package:foodapp/utils/app_colors.dart';
+import 'package:foodapp/utils/constants.dart';
+import 'package:foodapp/utils/util_function.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var _isObscure = true;
+  final _email = TextEditingController();
+  final _password = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: FadeInRight(
+        child: Container(
+          height: size.height,
+          color: Color(0xffE5E5E5),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                CustomHeader(
+                  size: size,
+                  header: "Login",
+                  tagline: 'Access Account',
+                  image: 'top.png',
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: kwhite,
+                            elevation: 2,
+                            padding: EdgeInsets.all(
+                              20,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                15,
+                              ),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Image.asset(
+                            Constants.imageAsset(
+                              'google.png',
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: kwhite,
+                            elevation: 2,
+                            padding: EdgeInsets.all(
+                              20,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                15,
+                              ),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Image.asset(
+                            Constants.imageAsset(
+                              'facebook.png',
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      'or Login with Email',
+                      style: GoogleFonts.poppins(
+                        fontSize: 15,
+                        color: greycolor,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 30,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Email',
+                            style: GoogleFonts.poppins(
+                              fontSize: 16,
+                              color: fontcolorblack,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 6,
+                          ),
+                          CustomTextField(
+                            controller: _email,
+                          ),
+                          Text(
+                            'Password',
+                            style: GoogleFonts.poppins(
+                              fontSize: 16,
+                              color: fontcolorblack,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(
+                            width: double.infinity,
+                            child: TextField(
+                              controller: _password,
+                              obscureText: _isObscure,
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: kwhite,
+                                suffixIcon: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _isObscure = !_isObscure;
+                                    });
+                                  },
+                                  icon: Icon(
+                                    _isObscure
+                                        ? Icons.visibility
+                                        : Icons.visibility_off,
+                                  ),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(
+                                    10,
+                                  ),
+                                  borderSide: BorderSide(
+                                    color: kwhite,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(
+                                    10,
+                                  ),
+                                  borderSide: BorderSide(
+                                    color: Colors.red,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 35,
+                          ),
+                          CutomButton(
+                            text: "Sign In",
+                            onTap: () {},
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Center(
+                            child: RichText(
+                              text: TextSpan(children: [
+                                TextSpan(
+                                  text: 'Don’t have an account?',
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 15,
+                                    color: greycolor,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: 'Register',
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () {
+                                      UtilFunction.navigateTo(
+                                          context, RegisterPage());
+                                    },
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 15,
+                                    color: fontcolorblack,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ]),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  bool inpuValidation() {
+    var isValid = false;
+    if (_email.text.isEmpty || _password.text.isEmpty) {
+      isValid = false;
+    } else if (EmailValidator.validate(_email.text)) {
+      isValid = false;
+    } else {
+      isValid = true;
+    }
+    return isValid;
+  }
+}
