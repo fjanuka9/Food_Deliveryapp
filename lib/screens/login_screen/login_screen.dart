0262668285546a4079c46b62c5a86fcb099b2846
fjanuka9 <90580551+fjanuka9@users.noreply.github.com>
2021-10-01 18:42:06 +0530

    up

--- lib/screens/login_screen/login_screen.dart
+++ lib/screens/login_screen/login_screen.dart
@@ -1,10 +1,14 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:email_validator/email_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:foodapp/components/custom_button.dart';
+import 'package:foodapp/components/custom_dialogbox.dart';
 import 'package:foodapp/components/custom_header.dart';
 import 'package:foodapp/components/custom_text_field.dart';
+import 'package:foodapp/controllers/auth_controller.dart';
 import 'package:foodapp/screens/login_screen/register_screen.dart';
 import 'package:foodapp/utils/app_colors.dart';
 import 'package:foodapp/utils/constants.dart';
@@ -22,6 +26,8 @@ class _LoginPageState extends State<LoginPage> {
   var _isObscure = true;
   final _email = TextEditingController();
   final _password = TextEditingController();
+
+  FirebaseAuth auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -175,7 +181,22 @@ class _LoginPageState extends State<LoginPage> {
                           ),
                           CustomButton(
                             text: "Sign In",
-                            onTap: () {},
+                            onTap: () async {
+                              if (inpuValidation()) {
+                                await AuthController().loginUser(
+                                  context,
+                                  _email.text,
+                                  _password.text,
+                                );
+                              } else {
+                                DialogBox().dialogBox(
+                                  context,
+                                  DialogType.ERROR,
+                                  'Incorrect information.',
+                                  'Please enter valid details',
+                                );
+                              }
+                            },
                           ),
                           SizedBox(
                             height: 20,
@@ -223,7 +244,7 @@ class _LoginPageState extends State<LoginPage> {
     var isValid = false;
     if (_email.text.isEmpty || _password.text.isEmpty) {
       isValid = false;
-    } else if (EmailValidator.validate(_email.text)) {
+    } else if (!EmailValidator.validate(_email.text)) {
       isValid = false;
     } else {
       isValid = true;
