6a09ec3ccc0257f9b72706da9862830991b53006
fjanuka9 <90580551+fjanuka9@users.noreply.github.com>
2021-10-07 17:26:53 +0530

    ad

--- lib/controllers/db_controller.dart
+++ lib/controllers/db_controller.dart
@@ -12,13 +12,15 @@ class DataBaseController {
     String name,
     String phonenumber,
     String email,
+    String uid,
   ) {
-    // Call the user's CollectionReference to add a new user
     return users
-        .add({
+        .doc('uid')
+        .set({
           'name': name,
           'phone': phonenumber,
           'email': email,
+          'uid': uid,
         })
         .then((value) => print("User Added"))
         .catchError((error) => print("Failed to add user: $error"));
