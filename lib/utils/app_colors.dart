8a56c54600380a01b0eb49d0fc54e47530ee86fc
fjanuka9 <90580551+fjanuka9@users.noreply.github.com>
2021-10-15 18:56:10 +0530

    ad

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,4 +3,4 @@ import 'package:flutter/material.dart';
 const primarycolor = Color(0xffFFD200);
 const greycolor = Color(0xff838383);
 const kwhite = Color(0xffFFFFFF);
-const fontcolorblack = Color(0xff373737);
+const kblack = Color(0xff373737);
